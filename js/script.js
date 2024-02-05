a4cbc7f178580b6de6ed6054b23a33aa9cbdaf97
ush9211 <pg.ush9211@gmail.com>
2024-02-05 18:09:04 +0900

    hero and main works

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
 $(function(){
-    $('')
-})
+    $('.gnb').hover(function(){
+        $('.lnb').fadeToggle();
+    });
+
+    $('.tabnav>li').click(function(e){
+        e.preventDefault();
+
+        $('.tabnav>li').removeClass('active');
+
+        const href = $(this).find('a').attr('href');
+
+        $(this).addClass('active');
+
+        $('.tab').removeClass('active');
+
+        $(href).addClass('active');
+    });
+});
